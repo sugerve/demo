18eba280819a3728f3f1dfa5b8e11255a168926c
daizhengxi <daizhengxi@example.com>
2020-08-25 18:00:17 +0800

    Initial commit

---
+++ DynamicWeb01/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jack
+  Date: 2020/8/25
+  Time: 16:45
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>动态Web</title>
+  </head>
+  <body>
+     <h1 style="color: red">动态111WebHello ,DynamicWeb!</h1>
+  </body>
+</html>
---
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+
+import java.util.ArrayList;
+
+//单行注释
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        ArrayList a = new ArrayList();
+        System.out.println("hello Worlds");
+        a.add(11);
+        for (Object o : a) {
+            
+        }
+
+        for (int i = 0; i < a.size(); i++) {
+            
+        }
+
+        for (int i = a.size() - 1; i >= 0; i--) {
+            
+        }
+
+        if (a == null) {
+
+        }
+        if (args != null) {
+
+        }
+
+
+    }
+
+
+}
